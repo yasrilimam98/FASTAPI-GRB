5563ff7c6163b51fbc75bc55d885371a4bc392f4
yasrilimam98 <yasrilimamofficial@gmail.com>
2021-11-19 10:42:24 +0700

    not yet solved cache query

--- config/database.py
+++ config/database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import configparser
+import pymysql.cursors,sys
 
 config = configparser.ConfigParser()
 config.read('alembic.ini')
@@ -15,3 +16,26 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 conn = engine.connect()
+
+# try:
+#     connmysql = pymysql.connect(
+#         user="root",
+#         password="",
+#         host="localhost",
+#         port=3308,
+#         database="grbclient"
+ 
+#     )
+#     print(f"Connected")   
+# except pymysql.Error as e:
+#     print(f"Error connecting to MariaDB Platform: {e}")
+#     sys.exit(1)
+# # Base = declarative_base()
+# querymsql1 = connmysql.cursor()
+# querymsql1.close()
+
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3308/grbclient"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+# querymysql = engine.connect()
--- routes/client.py
+++ routes/client.py
@@ -1,21 +1,27 @@
+from pymysql import cursors
 from schemas.client import ClientSchema, Clients, ClientRegno
 from models.client import Client
 from fastapi import APIRouter, Response, status, FastAPI, Query
 from config.database import conn
 
-
 client = APIRouter() 
 
 
-@client.get('/Client/{regno}', 
+@client.get('/Client/', 
              description="Masukan data Registrasi & Serial")
 async def regno(regno: int, serial: str, response: Response):
-    query = Client.select().where(Client.c.regno == regno and Client.c.serial == serial)
-    data = conn.execute(query).fetchone()
+    # query = Client.select().where(Client.c.regno == regno and Client.c.serial == serial)
+    query = """SELECT CASE WHEN DATEDIFF(duedate,curdate()) < 0 THEN 0 ELSE 1 END as statuslisensi, DATEDIFF(duedate,curdate()) as jmlhari,dbname,dbport,localnetwork,localport,publicnetwork,publicport,`name` FROM mclient Where regno = %s AND serial = %s"""
+    data = conn.execute(query,(regno),(serial)).fetchone()
+    
     if data is None:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message" : "Masa Registrasi Anda telah berakhir", "status": response.status_code}
-    response = {"message": f"Masa Registrasi anda berlaku {regno}", "status": data}
+        return {"message" : "periksa kembali idclient dan serial", "status": response.status_code}
+    else : 
+         if data.statuslisensi == 1:
+            response = {"message": f"Masa Registrasi anda berlaku", "status": data.statuslisensi}
+         else : 
+            response = {"message": f"Masa Registrasi anda berakhir", "status": data} 
     return response
 
 @client.get('/Client/{id}',
